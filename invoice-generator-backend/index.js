a31deeb407fbd8e832cf06a9565301c9f4fe6855
Sameer Ali <sameersheikh0288@gmail.com>
2024-06-02 11:52:10 +0530

    u

--- invoice-generator-backend/index.js
+++ invoice-generator-backend/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const http = require('http');
-const cookieParser = require('cookie-parser');
+const http =require("http");
+const cookieParser = require("cookie-parser");
 const dotenv = require('dotenv');
+
 
 dotenv.config();
 
@@ -12,24 +13,17 @@ const productRoutes = require('./routes/productRoutes');
 const invoiceRoutes = require('./routes/invoiceRoutes');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use(cookieParser());
 
-// CORS Configuration
-const corsOptions = {
-  origin: 'https://invoice-generator-frontend-iota.vercel.app', // Update with your frontend URL
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: 'Content-Type,Authorization',
-  credentials: true, // Set to true if your requests include cookies or authentication headers
-};
-
-app.use(cors(corsOptions));
-
-// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/invoices', invoiceRoutes);
+
+
 
 const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
@@ -43,3 +37,4 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log({ err });
   process.exit(1);
 });
+
