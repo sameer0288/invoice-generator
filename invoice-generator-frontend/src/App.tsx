3f9bf235282eeaa802e9bdf9d5b19484dc281ad9
Sameer Ali <sameersheikh0288@gmail.com>
2024-06-02 10:02:23 +0530

    updates

--- invoice-generator-frontend/src/App.tsx
+++ invoice-generator-frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/index';
