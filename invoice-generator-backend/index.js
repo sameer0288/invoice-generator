6648640d8a59977f9275afbe09244332844a7b18
Sameer Ali <sameersheikh0288@gmail.com>
2024-06-02 11:40:14 +0530

    new

--- invoice-generator-backend/index.js
+++ invoice-generator-backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+// const cors = require('cors');
 const http =require("http");
 const cookieParser = require("cookie-parser");
 const dotenv = require('dotenv');
@@ -14,7 +14,7 @@ const invoiceRoutes = require('./routes/invoiceRoutes');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+// app.use(cors());
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
