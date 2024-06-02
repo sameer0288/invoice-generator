7ed0e4b685fc2f09a6e4c2cebe81af36573c7c8e
Sameer Ali <sameersheikh0288@gmail.com>
2024-06-02 11:27:08 +0530

    new

--- invoice-generator-backend/index.js
+++ invoice-generator-backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const http =require("http");
+const http = require("http");
 const cookieParser = require("cookie-parser");
 const dotenv = require('dotenv');
 
@@ -14,14 +14,21 @@ const invoiceRoutes = require('./routes/invoiceRoutes');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
 app.use(cookieParser());
+
+// Update CORS configuration
+const corsOptions = {
+  origin: 'https://invoice-generator-frontend-seven.vercel.app', // Update with your frontend URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/invoices', invoiceRoutes);
-
-
 
 const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
@@ -35,4 +42,3 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log({ err });
   process.exit(1);
 });
-
