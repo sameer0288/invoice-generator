118c2c081527d129f08e0c1f53f54be06e116215
Sameer Ali <sameersheikh0288@gmail.com>
2024-06-02 11:46:47 +0530

    new-backend

--- invoice-generator-backend/index.js
+++ invoice-generator-backend/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const http =require("http");
-const cookieParser = require("cookie-parser");
+const http = require('http');
+const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -14,14 +14,22 @@ const invoiceRoutes = require('./routes/invoiceRoutes');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
 app.use(cookieParser());
 
+// CORS Configuration
+const corsOptions = {
+  origin: 'https://invoice-generator-frontend-iota.vercel.app', // Update with your frontend URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials: true, // Set to true if your requests include cookies or authentication headers
+};
+
+app.use(cors(corsOptions));
+
+// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/invoices', invoiceRoutes);
-
-
 
 const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
@@ -35,4 +43,3 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log({ err });
   process.exit(1);
 });
-
